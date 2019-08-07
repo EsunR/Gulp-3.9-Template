cf9669bc4646314a7275ac5aaa17a5da57b734ef
EsunR <641411169@qq.com>
2019-08-07 13:08:07 +0800

    哈希化命名

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 (function () {
-    console.log("1");
+  var app = document.querySelector('#app');
+  var oImg = document.createElement("img");
+  oImg.src = "./images/1.jpg";
+  app.appendChild(oImg);
+  console.log(1);
 })()
